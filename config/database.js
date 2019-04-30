ff98f3554f85f6de20b1c442c0153fec600082b0
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2019-04-30 10:54:09 +0700

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -5,6 +5,6 @@ const MONGO_PORT = process.env.MONGO_PORT || 27017;
 
 const mongoUrl = `mongodb://${MONGO_PATH}:${MONGO_PORT}/hello`;
 
-//const mongoUrl = "mongodb://localhost/local";
+// const mongoUrl = `mongodb://localhost/local`;
 
 exports.mongoUrl = mongoUrl;
