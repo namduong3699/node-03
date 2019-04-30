af257566e25cf619613d558cfef36f20a02b1843
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2019-04-30 10:25:12 +0700

    ver 1.2

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@
 const MONGO_PATH = process.env.MONGO_PATH || 'localhost';
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 
-const mongoUrl = "mongodb://${MONGO_PATH}:${MONGO_PORT}/hello";
+const mongoUrl = `mongodb://${MONGO_PATH}:${MONGO_PORT}/hello`;
 
 //const mongoUrl = "mongodb://localhost/local";
 
